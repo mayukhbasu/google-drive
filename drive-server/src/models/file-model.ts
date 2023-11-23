0caf441030c1fca39a216530ce3ee2858ca11473
Mayukh Basu <roxy.firebird@gmail.com>
2023-11-23 22:13:57 +0530

    code cleanup

--- drive-server/src/models/file-model.ts
+++ drive-server/src/models/file-model.ts
@@ -2,11 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 export interface IFile extends Document {
   originalname: string;
-  encoding: string;
-  mimetype: string;
-  destination: string;
   filename: string;
-  path: string;
   size: number;
   uploaderName: string;
   uploadDate: Date;
@@ -14,13 +10,9 @@ export interface IFile extends Document {
 
 const FileSchema: Schema = new Schema({
   originalname: { type: String, required: true, unique: true },
-  encoding: { type: String, required: false },
-  mimetype: { type: String, required: false },
-  destination: { type: String, required: false },
   filename: { type: String, required: true },
-  path: { type: String, required: false },
   size: { type: Number, required: true},
-  uploaderName: {type: String, required: false},
+  uploaderName: {type: String, required: true},
   uploadDate: { type: Date, default: Date.now }
 });
 
